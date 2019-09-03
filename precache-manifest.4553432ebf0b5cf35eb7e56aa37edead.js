26d984032afc3c9c5172367cfcb58ac722063a8d
hazemsalim <haz.future@gmail.com>
2019-09-03 10:11:34 +0300

    Updates

---
+++ precache-manifest.4553432ebf0b5cf35eb7e56aa37edead.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba36b2aaca486017316698e9f57ac67a",
+    "url": "/booksList/index.html"
+  },
+  {
+    "revision": "eca3e3627b37a2d38662",
+    "url": "/booksList/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "69ae977287dd65b884b5",
+    "url": "/booksList/static/js/2.aa8f5869.chunk.js"
+  },
+  {
+    "revision": "eca3e3627b37a2d38662",
+    "url": "/booksList/static/js/main.6c23db2b.chunk.js"
+  },
+  {
+    "revision": "58c273884891f64d2d42",
+    "url": "/booksList/static/js/runtime~main.94b234ae.js"
+  }
+]);
